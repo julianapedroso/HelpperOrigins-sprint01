036c4ebcab39c832704f38276c0745a0d697309c
julianapedroso <julia.cristina@hotmail.com.br>
2021-11-08 18:26:57 -0300

    fix(exercicio2-functional): funções e variáveis tipadas e reorganizadas no paradigma funcional

--- exercicio2/exercicio2-functional.js
+++ exercicio2/exercicio2-functional.js
@@ -22,27 +22,32 @@ var listFuncional = [
 ];
 /* FUNCIONAL */
 // a) Crie uma função que retorne a bio do id passado
-var getBioByIdFuncional = function (id) {
-    return listFuncional.find(function (person) { return person.id === id; }).bio;
+var getBioByIdFuncional = function (id, list) {
+    var _a;
+    var findBioPerson = list.find(function (person) { return person.id === id; });
+    return (_a = findBioPerson === null || findBioPerson === void 0 ? void 0 : findBioPerson.bio) !== null && _a !== void 0 ? _a : 'Id não encontrado';
 };
-console.log({ bio: getBioByIdFuncional(4) });
+console.log({ bio: getBioByIdFuncional(4, listFuncional) });
 // b) Crie uma função que retorne o name do id passado
-var getNameByIdFuncional = function (id) {
-    return listFuncional.find(function (person) { return person.id === id; }).name;
+var getNameByIdFuncional = function (id, list) {
+    var _a;
+    var findNamePerson = list.find(function (person) { return person.id === id; });
+    return (_a = findNamePerson === null || findNamePerson === void 0 ? void 0 : findNamePerson.name) !== null && _a !== void 0 ? _a : 'Id não encontardo';
 };
-console.log({ name: getNameByIdFuncional(2) });
+console.log({ name: getNameByIdFuncional(2, listFuncional) });
 // c) Crie uma função que apague um item da lista a partir de um id passado
-var deleteItemByIdFuncional = function (id) {
-    listFuncional.splice(listFuncional.findIndex(function (item) { return item.id === id; }), 1);
+var deleteItemByIdFuncional = function (id, list) {
+    var deleteById = list.splice(list.findIndex(function (item) { return item.id === id; }), 1);
+    return deleteById;
 };
 console.log("old listFuncional:", listFuncional);
-console.log(deleteItemByIdFuncional(2));
+console.log(deleteItemByIdFuncional(1, listFuncional));
 console.log("new listFuncional:", listFuncional);
 // d) Crie uma função que altere a bio ou o name a partir de um id passado
-var updateItemByIdFuncional = function (id, name, bio) {
-    var objectIndex = listFuncional.findIndex(function (item) { return item["id"] === id; });
-    name ? listFuncional[objectIndex]["name"] = name : null;
-    bio ? listFuncional[objectIndex]["bio"] = bio : null;
+var updateItemByIdFuncional = function (id, list, name, bio) {
+    var objectIndex = list.findIndex(function (item) { return item.id === id; });
+    list[objectIndex]['name'] = name !== null && name !== void 0 ? name : null;
+    list[objectIndex]['bio'] = bio !== null && bio !== void 0 ? bio : null;
 };
-console.log(updateItemByIdFuncional(4, "Squad Delta", "Squad Delta é o melhor e mais incrível de todos <3"));
+console.log(updateItemByIdFuncional(4, listFuncional, "Mudança no nome", "Squad Delta é o melhor e mais incrível de todos <3"));
 console.log("new listFuncional:", listFuncional);
--- exercicio2/exercicio2-functional.ts
+++ exercicio2/exercicio2-functional.ts
@@ -30,36 +30,36 @@ const listFuncional: Array<Person> = [
 /* FUNCIONAL */
 
 // a) Crie uma função que retorne a bio do id passado
-const getBioByIdFuncional = (id: number): string => {
-    return listFuncional.find(person => person.id === id).bio;
+const getBioByIdFuncional = (id: number, list: Array<Person>): string | undefined => {
+    const findBioPerson: Person | undefined = list.find((person: Person) => person.id === id)
+    return findBioPerson?.bio ?? 'Id não encontrado';
 }
 
-console.log({ bio: getBioByIdFuncional(4) });
+console.log({ bio: getBioByIdFuncional(4, listFuncional) });
   
 // b) Crie uma função que retorne o name do id passado
-const getNameByIdFuncional = (id: number): string => {
-    return listFuncional.find(person => person.id === id).name;
+const getNameByIdFuncional = (id: number, list: Array<Person>): string | undefined => {
+    const findNamePerson: Person | undefined = list.find((person:Person) => person.id === id)
+    return findNamePerson?.name ?? 'Id não encontardo';
 }
 
-console.log({ name: getNameByIdFuncional(2) });
+console.log({ name: getNameByIdFuncional(2, listFuncional) });
 
 // c) Crie uma função que apague um item da lista a partir de um id passado
-const deleteItemByIdFuncional = (id:number): void => {
-    listFuncional.splice(listFuncional.findIndex(item => item.id === id), 1);
+const deleteItemByIdFuncional = (id:number, list: Array<Person>): object => {
+    const deleteById: object = list.splice(list.findIndex(item => item.id === id), 1);
+    return deleteById;
 }
-
 console.log("old listFuncional:", listFuncional);
-console.log(deleteItemByIdFuncional(2));
+console.log(deleteItemByIdFuncional(1, listFuncional));
 console.log("new listFuncional:", listFuncional);
 
 // d) Crie uma função que altere a bio ou o name a partir de um id passado
-const updateItemByIdFuncional = (id: number, name?: string, bio?: string): void => {
-    const objectIndex = listFuncional.findIndex(item => item["id"] === id);
-    
-    name ? listFuncional[objectIndex]["name"] = name : null;
-
-    bio ? listFuncional[objectIndex]["bio"] = bio : null;
+const updateItemByIdFuncional = (id: number, list: Array<Person>, name?: string, bio?: string): void => {
+    const objectIndex: number = list.findIndex(item => item.id === id);
+    list[objectIndex]['name'] = name ?? null;
+    list[objectIndex]['bio'] = bio ?? null;
 }
 
-console.log(updateItemByIdFuncional(4, "Squad Delta", "Squad Delta é o melhor e mais incrível de todos <3" ));
+console.log(updateItemByIdFuncional(4, listFuncional, "Mudança no nome", "Squad Delta é o melhor e mais incrível de todos <3" ));
 console.log("new listFuncional:", listFuncional);
